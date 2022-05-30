39073c80fa7234287108ba31258cd4ea55bacd6e
redzplayz <32217517+redzplayz@users.noreply.github.com>
2022-05-30 16:14:13 +0100

    Made AI script

---
+++ Project/GamesJamMay/Assets/HarryScripts/AIWander.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AIWander : MonoBehaviour
+{
+	[SerializeField] private NavMeshAgent agent;
+	[SerializeField] private Transform player;
+	[SerializeField] private LayerMask whatIsGround, whatIsPlayer;
+
+	[SerializeField] private Vector3 walkPoint;
+	[SerializeField] private bool walkPointSet;
+	[SerializeField] private float walkPointRange;
+	[SerializeField] private Vector3 distanceToWalkPoint;
+	[SerializeField] private float rangeCheck = 2f;
+	private Color color;
+	private void Awake()
+	{
+		color = Random.ColorHSV();
+		StartCoroutine(RunPatrol());
+	}
+
+	private IEnumerator RunPatrol()
+	{
+		while (true)
+		{
+			Patrolling();
+			yield return new WaitForSeconds(.1f);
+		}
+	}
+	private void Patrolling()
+	{
+		if (!walkPointSet)
+		{
+			SearchWalkPoint();
+		}
+		if(walkPointSet)
+		{
+			agent.SetDestination(walkPoint);
+		}
+		distanceToWalkPoint = transform.position - walkPoint;
+		if(distanceToWalkPoint.magnitude < rangeCheck)
+		{
+			walkPointSet = false;
+		}
+	}
+	private void SearchWalkPoint()
+	{
+		float randomZ = Random.Range(-walkPointRange, walkPointRange);
+		float randomX = Random.Range(-walkPointRange, walkPointRange);
+		
+		walkPoint = new Vector3(transform.position.x + randomX, 1, transform.position.z + randomZ);
+		NavMeshHit hit;
+		if (NavMesh.SamplePosition(walkPoint, out hit, 1.0f, NavMesh.AllAreas))
+		{
+			walkPointSet = hit.hit;
+		}
+		else
+		{
+			SearchWalkPoint();
+		}
+	}
+	private void OnDrawGizmos()
+	{
+		Gizmos.color = color;
+		Gizmos.DrawSphere(walkPoint, .2f);
+	}
+}
